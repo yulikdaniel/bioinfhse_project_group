fd5783144f1f8c7df805a9551f390d9360cce8b5
Yuliy Daniel <yulik.daniel@gmail.com>
2023-06-08 01:17:25 +0300

    final commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Вводная презентация: https://docs.google.com/presentation/d/1772P7ESvWNoROx7vQfoczJRTwY91pKTaoeY_qaRDiYA/edit?usp=sharing
+
+Итоговая презентация: https://docs.google.com/presentation/d/1BLV2kfz2SPYmkSJ9LCavfTp7Twm2-95eyKq7SX0GcFQ/edit?usp=sharing
--- change.sh
+++ change.sh
@@ -1,4 +1,9 @@
 for thing in "human" "mouse" "zebrafish" "drosophila" "c.elegans" "yeast" "ciliate" "methanocaldococcus" "thermococcus" "e.coli" "tuberculosis"
 do
-mv results/SCARCA4_$thing.blast results/SCARCA4-$thing.blast
+mv results/SCARCA4_$thing.blast results/SMARCA4-$thing.blast
+done
+
+for thing in "human" "mouse" "zebrafish" "drosophila" "c.elegans" "yeast" "ciliate" "methanocaldococcus" "thermococcus" "e.coli" "tuberculosis"
+do
+mv results/P300-$thing.blast results/EP300-$thing.blast
 done
