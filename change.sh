bf20161e1fb06487e53860514ec2371631fc0e22
Yuliy Daniel <yulik.daniel@gmail.com>
2023-06-07 19:00:01 +0300

    initial commit

---
+++ change.sh
@@ -0,0 +1,4 @@
+for thing in "human" "mouse" "zebrafish" "drosophila" "c.elegans" "yeast" "ciliate" "methanocaldococcus" "thermococcus" "e.coli" "tuberculosis"
+do
+mv results/SCARCA4_$thing.blast results/SCARCA4-$thing.blast
+done
