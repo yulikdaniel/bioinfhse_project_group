1025684f3c6f76c9d7abbcc130a42d45c25cfc8b
Yuliy Daniel <yulik.daniel@gmail.com>
2023-06-08 11:44:04 +0300

    Superfinal changes

--- change.sh
+++ change.sh
@@ -1,9 +1,17 @@
-for thing in "human" "mouse" "zebrafish" "drosophila" "c.elegans" "yeast" "ciliate" "methanocaldococcus" "thermococcus" "e.coli" "tuberculosis"
+# for thing in "human" "mouse" "zebrafish" "drosophila" "c.elegans" "yeast" "ciliate" "methanocaldococcus" "thermococcus" "e.coli" "tuberculosis"
+# do
+# mv results/SCARCA4_$thing.blast results/SMARCA4-$thing.blast
+# done
+
+for hist in "H2A" "H2B" "H3" "H4"
 do
-mv results/SCARCA4_$thing.blast results/SMARCA4-$thing.blast
+    for thing in "human" "mouse" "zebrafish" "drosophila" "c.elegans" "yeast" "ciliate" "methanocaldococcus" "thermococcus" "e.coli" "tuberculosis"
+    do
+    mv results/"$hist"_$thing.blast results/$hist-$thing.blast
+    done
 done
 
-for thing in "human" "mouse" "zebrafish" "drosophila" "c.elegans" "yeast" "ciliate" "methanocaldococcus" "thermococcus" "e.coli" "tuberculosis"
-do
-mv results/P300-$thing.blast results/EP300-$thing.blast
-done
+# for thing in "human" "mouse" "zebrafish" "drosophila" "c.elegans" "yeast" "ciliate" "methanocaldococcus" "thermococcus" "e.coli" "tuberculosis"
+# do
+# mv results/P300-$thing.blast results/EP300-$thing.blast
+# done
